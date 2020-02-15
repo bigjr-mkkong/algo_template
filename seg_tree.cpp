ca6c04b612bb6098a70a4d260de5ba010daa9b80
kongboming <48128050+kongboming@users.noreply.github.com>
2020-02-15 22:53:20 +0800

    线段树
    查询O(logn)
    可以持久化(虽然我还没写)
    还可以变成毒瘤zkw线段树(详见《统计的力量》)

---
+++ seg_tree.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+const int maxn=100000+10;
+struct node{
+	ll sum,tag;
+}t[maxn*2];
+ll arr[maxn];
+ll ans=0;
+void update(ll pos){
+	t[pos].sum=t[pos<<1].sum+t[pos<<1|1].sum;
+}
+void build(ll l,ll r,ll pos){
+	if(l==r){
+		t[pos].sum=arr[l];
+		return;
+	}
+	ll mid=(l+r)/2;
+	build(l,mid,pos<<1);
+	build(mid+1,r,pos<<1|1);
+	update(pos);
+}
+void f(ll pos,ll l,ll r,ll k)
+{
+    t[pos].tag=t[pos].tag+k;
+    t[pos].sum=t[pos].sum+k*(r-l+1);
+}
+void pushdown(ll l,ll r,ll pos){
+	if(!t[pos].tag) return;
+	ll mid=(l+r)/2;
+	f(pos<<1,l,mid,t[pos].tag);
+	f(pos<<1|1,mid+1,r,t[pos].tag);
+	t[pos].tag=0;
+}
+void add(ll tl,ll tr,ll l,ll r,ll v,ll pos){
+	if(tl<=l&&tr>=r){
+		t[pos].sum+=v*(r-l+1);
+		t[pos].tag+=v;
+		return;
+	}
+	if(r<tl||l>tr){
+		return;
+	}
+	ll mid=(l+r)/2;
+	pushdown(l,r,pos);
+	add(tl,tr,l,mid,v,pos<<1);
+	add(tl,tr,mid+1,r,v,pos<<1|1);
+	update(pos);
+}
+void query(ll tl,ll tr,ll l,ll r,ll pos){
+	if(tl<=l&&tr>=r){
+		ans+=t[pos].sum;
+		return;
+	}
+	if(r<tl||l>tr){
+		return;
+	}
+	ll mid=(l+r)/2;
+	pushdown(l,r,pos);
+	query(tl,tr,l,mid,pos<<1);
+	query(tl,tr,mid+1,r,pos<<1|1);
+	return;
+}
+int main(void){
+	ios::sync_with_stdio(false);
+	ll n,m;
+	cin>>n>>m;
+	for(int i=1;i<=n;i++){
+		cin>>arr[i];
+	}
+	build(1,n,1);
+	for(int i=1;i<=m;i++){
+		ll opt,x,y,k;
+		cin>>opt;
+		if(opt==1){
+			cin>>x>>y>>k;
+			add(x,y,1,n,k,1);
+		}else{
+			cin>>x>>y;
+			query(x,y,1,n,1);
+			printf("%lld\n",ans);
+			ans=0;
+		}
+	}
+}
