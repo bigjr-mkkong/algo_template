e37982678bb75e6a3054c599c979d81d3ea217f6
kongboming <48128050+kongboming@users.noreply.github.com>
2020-02-27 23:00:59 +0800

    左偏树

--- leftist tree.cpp
+++ leftist tree.cpp
@@ -1,76 +1,54 @@
+//luogu p3377
 #include <bits/stdc++.h>
 using namespace std;
-const int maxn=19260817;
-int ch[maxn][2],fa[maxn],dis[maxn],val[maxn];
-int inline read()
-{
-    int x=0,f=1;char ch=getchar();
-    while(ch<'0'||ch>'9'){if(ch=='-')f=-1;ch=getchar();}
-    while(ch>='0'&&ch<='9'){x=x*10+ch-'0';ch=getchar();}
-    return x*f;
-}
-void swap(int &x,int &y){
-	int t=x;
-	x=y;
-	y=t;
-	return;
-}
+const int maxn=1e5+5;
+struct node{
+	int val,l,r,dis,fa;
+}ltt[maxn];
+int n,m;
 int merge(int x,int y){
-	if(x==0||y==0){
-		return x+y;
-	}
-	if(val[x]>val[y]||(val[x]==val[y]&&x>y)){
-		swap(x,y);
-	}
-	ch[x][1]=merge(ch[x][1],y);
-	fa[ch[x][1]]=x;
-	if(dis[ch[x][0]]<dis[ch[x][1]]){
-		swap(ch[x][0],ch[x][1]);
-	}
-	dis[x]=dis[ch[x][1]]+1;
+	if(!x||!y) return x+y;
+	if(ltt[x].val>ltt[y].val||(ltt[x].val==ltt[y].val&&x>y)) swap(x,y);
+	ltt[x].r=merge(ltt[x].r,y);
+	ltt[ltt[x].r].fa=x;
+	if(ltt[ltt[x].l].dis<ltt[ltt[x].r].dis) swap(ltt[x].l,ltt[x].r);
+	ltt[x].dis=ltt[ltt[x].r].dis+1;
 	return x;
+}
+int find(int x){
+	return ltt[x].fa==x?x:ltt[x].fa=find(ltt[x].fa);
 }
 void pop(int x){
-	val[x]=-1;
-	fa[ch[x][0]]=fa[ch[x][1]]=0;
-	merge(ch[x][0],ch[x][1]);
-}
-int getf(int x){
-	while(fa[x]!=0){
-		x=fa[x];
-	}
-	return x;
+	ltt[x].val=-1;
+	ltt[ltt[x].l].fa=ltt[x].l;
+	ltt[ltt[x].r].fa=ltt[x].r;
+	ltt[x].fa=merge(ltt[x].l,ltt[x].r);
 }
 int main(void){
-	int m,n;
-	n=read(),m=read();
-    dis[0]=-1;
-    for (int i=1;i<=n;i++)
-        val[i]=read();
-    for (int i=1;i<=m;i++)
-    {
-        int com=read();
-        if (com==1)
-        {
-            int x=read(),y=read();
-            if (val[x]==-1 || val[y]==-1)
-                continue;
-            if (x==y)
-                continue;
-            int fx=getf(x),fy=getf(y);
-            merge(fx,fy);
-        }
-        else
-        {
-            int x=read();
-            if (val[x]==-1)
-                puts("-1");
-            else
-            {
-                int y=getf(x);
-                printf("%d\n",val[y]);
-                pop(y);
-            }
-        }
-    }
+	scanf("%d %d",&n,&m);
+	ltt[0].dis=-1;
+	for(int i=1;i<=n;i++){
+		scanf("%d",&ltt[i].val);
+		ltt[i].fa=i;
+	}
+	while(m--){
+		int opt,x,y;
+		scanf("%d",&opt);
+		if(opt==1){
+			scanf("%d %d",&x,&y);
+			if(ltt[x].val==-1||ltt[y].val==-1) continue;
+			int x1=find(x),y1=find(y);
+			if(x1==y1) continue;
+			ltt[x1].fa=ltt[y1].fa=merge(x1,y1);
+		}else{
+			scanf("%d",&x);
+			if(ltt[x].val==-1){
+				printf("-1\n");
+			}else{
+				int x1=find(x);
+				printf("%d\n",ltt[x1].val);
+				pop(x1);
+			}
+		}
+	}
 }
